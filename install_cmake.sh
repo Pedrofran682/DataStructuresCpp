4e5eb1aa706cdb570fda7a1a7cba99b69475acf1
Pedro França <41302844+Pedrofran682@users.noreply.github.com>
2025-03-16 16:28:49 -0300

    Env setup (#1)
    * initial config

---
+++ install_cmake.sh
@@ -0,0 +1,5 @@
+sudo apt install build-essential libtool autoconf unzip wget
+sudo apt-get -y install cmake
+sudo pip install cmake --upgrade
+sudo apt-get -y install make
+sudo apt-get install -y gcc
