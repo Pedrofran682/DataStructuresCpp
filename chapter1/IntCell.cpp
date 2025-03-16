0f03ddd87a6ce2c61c2ad90f82bb2d424a0fa248
pmourafr <pedrofran682@gmail.com>
2025-03-16 19:27:56 +0000

    initial config

--- chapter1/IntCell.cpp
+++ chapter1/IntCell.cpp
@@ -1,6 +1,16 @@
 
 
-class IntCell
-{
+class IntCell {
+public:
+  IntCell() { storedValue = 0; }
 
-}
+  IntCell(int initialValue) { storedValue = initialValue; }
+
+  int read() { return storedValue; }
+
+  void write(int x) { storedValue = x; }
+
+
+private:
+  int storedValue;
+};
